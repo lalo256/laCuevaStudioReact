76aef0f7d40ef767cedd625e066a2f5bd81e472e
Eduardo Cuevas <alucardlalo.1@gmail.com>
2020-12-13 13:56:49 -0600

    finalizacion de pantalla home

---
+++ src/component/FooterCompo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Insta from '../Images/instagram-brands.svg';
+import './style/FooterCompo.css';
+
+class FooterCompo extends React.Component{
+    render() {
+        return(
+            <React.Fragment>
+                <div className="container fontFooter">
+                    <p>Copyright © Todos los Derechos Reservados. </p>
+                    <a href="https://www.instagram.com"><img className="instaImg" src={Insta}/></a>
+                </div>
+                <br/><br/>
+            </React.Fragment>
+        )
+    }
+}
+export default FooterCompo;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import carru1 from '../Images/0451.jpeg'
 import carru2 from '../Images/0452.jpeg'
 import carru3 from '../Images/0453.jpeg'
 import carru4 from '../Images/0454.jpeg'
+import FooterCompo from '../component/FooterCompo';
 
 class Home extends React.Component{
     render() {
@@ -17,34 +18,26 @@ class Home extends React.Component{
                         <Carousel.Item interval={3000}>
                             <img className="imgcarusel" src={carru1} />
                             <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
-                        <Carousel.Item interval={3000}>
+                        <Carousel.Item>
                             <img className="imgcarusel" src={carru2}/>
                             <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
                             <img className="imgcarusel" src={carru3}/>
                             <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
                             <img className="imgcarusel" src={carru4}/>
                             <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
                 </div>
-
+                <FooterCompo />
             </React.Fragment>
         )
     }
