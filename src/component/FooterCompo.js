dd3d31c9ee4506af8e84f541956aba2b8d18544f
Eduardo Cuevas <alucardlalo.1@gmail.com>
2021-06-27 18:14:26 -0500

    fix href

--- src/component/FooterCompo.js
+++ src/component/FooterCompo.js
@@ -13,10 +13,10 @@ export default function FooterCompo(props) {
                         <a href="https://www.instagram.com/lacuevastudio.arquitectos/">
                             <li className={props.clase1? props.clase1:"ftletra fontposition1"}>Instagram</li>
                         </a>
-                        <a href="https://www.pinterest.com.mx/lacuevastudio_Arq/_saved/">
+                        <a href="https://www.linkedin.com/company/la-cueva-studio/?viewAsMember=true">
                             <li className={props.clase2? props.clase2:"ftletra fontposition2"}>linkedin</li>
                         </a>
-                        <a href="https://www.linkedin.com/company/la-cueva-studio/?viewAsMember=true">
+                        <a href="https://www.pinterest.com.mx/lacuevastudio_Arq/_saved/">
                             <li className={props.clase3? props.clase3:"ftletra fontposition3"}>pinterest</li>
                         </a>
                     </ul>
