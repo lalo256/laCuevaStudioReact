325f83941907ee280e60c267b1204eff54d8f91e
Alucardlalo <68416190+lalo256@users.noreply.github.com>
2021-09-04 20:43:30 -0500

    update productos secction

--- src/pages/Producto.js
+++ src/pages/Producto.js
@@ -4,33 +4,37 @@ import FooterCompo from "../component/FooterCompo";
 import '../component/style/producto.css';
 import pdf from '../Images/PDF001.jpg';
 
-class Producto extends React.Component{
+class Producto extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            prueba:1,
+            prueba: 1,
         }
     }
+
     render() {
         return (
             <React.Fragment>
-                <Navbar /><br/><br/><br/><br/>
-                <div className="row" style={{height:"100%"}}>
-                    <div className="container">
-                        <div className="col-4">
-                            <h6 className="titleProduct1">Mini-guia</h6>
-                            <a href="/producto/pdf" target="_blank">
-                            <img src={pdf} className="ImgProduct" alt={"fd"}/>
-                            </a>
+                <Navbar/><br/><br/><br/><br/>
+                <div className="" style={{height: "100%", width: "100%"}}>
+                    <div style={{height:"400px"}}>
+                        <div className="container-fluid">
+                            <div className="">
+                                <h6 className="titleProduct1">Mini-guia</h6>
+                                <a href="/producto/pdf" target="_blank">
+                                    <img src={pdf} className="ImgProduct" alt={"fd"}/>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <br/>
-                <div style={{bottom:"0"}}>
-                    <FooterCompo />
+                <div style={{bottom: "0"}}>
+                    <FooterCompo/>
                 </div>
             </React.Fragment>
         )
     }
 }
+
 export default Producto;
